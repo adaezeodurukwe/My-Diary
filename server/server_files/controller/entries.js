4b6431a39207e7d54a680e657410380bc8a1b5ed
Adaeze Odurukwe <daizyodurukwe@gmail.com>
2018-07-20 21:33:27 +0100

    delete entry endpoint

--- server/server_files/controller/entries.js
+++ server/server_files/controller/entries.js
@@ -3,16 +3,37 @@ import entries from '../model/entries';
  * @class entries
  */
 class Entries {
-    /**
-     * @returns {Object} recipes
-     * @param {*} req
-     * @param {*} res
-     */
-    static getEntries(req, res) {
-      return res.json({
-        entries
-      });
+  /**
+   * @returns {Object} recipes
+   * @param {*} req
+   * @param {*} res
+   */
+  static getEntries(req, res) {
+    return res.json({
+      entries
+    });
+  }
+  /**
+   * @returns {object} removeEcipes
+   * @param {*} req
+   * @param {*} res
+   */
+  static deleteEntry(req, res) {
+    let i = 0;
+    while(i < entries.length){
+      if (entries[i].id === parseInt(req.params.id, 10)) {
+        entries.splice(i, 1);
+        return res.json({
+          message: 'entry removed',
+        });
+      }
+      i++;
     }
+    return res.status(404).json({
+      message: 'entry not found',
+      error: true
+    });
+  }
 }
 
 export default Entries
