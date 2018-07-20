5275f814f08da787307f3d2313f33abc737a4500
Adaeze Odurukwe <daizyodurukwe@gmail.com>
2018-07-20 19:38:46 +0100

    Add entry endpoint

--- server/server_files/controller/entries.js
+++ server/server_files/controller/entries.js
@@ -13,6 +13,20 @@ class Entries {
         entries
       });
     }
+    static createEntry(req, res) {
+        entries.push({
+          id: entries.length + 1,
+          title: req.body.title,
+          content: req.body.content,
+          date_created: req.body.date_created,
+          modified: 0
+        });
+        return res.json({
+          entries,
+          message: 'new entry created',
+          error: false
+        });
+      }
 }
 
 export default Entries
--- server/server_files/index.js
+++ server/server_files/index.js
@@ -1,11 +1,15 @@
 //import entries from './model/entries';
 import Entries from './controller/entries'
+import bodyParser from 'body-parser';
 
 const express = require('express');
 const app = express();
+app.use(bodyParser.json());
+
 
 app.get('/', (req, res)=>{
     res.send('Hello Ada')
 });
 app.get('/api/v1/entries', Entries.getEntries);
+app.post('/api/v1/entries', Entries.createEntry);
 app.listen(5000, () => console.log('this works') );
