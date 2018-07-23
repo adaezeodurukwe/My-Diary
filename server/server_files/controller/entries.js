03e083711a1a21ea2b1fb633834fbbf38fb6503e
Eze-odurukwe Adaeze <daizyodurukwe@gmail.com>
2018-07-23 07:42:56 +0100

    Update entries.js

--- server/server_files/controller/entries.js
+++ server/server_files/controller/entries.js
@@ -14,7 +14,7 @@ class Entries {
     });
   }
   /**
-   * @returns {object} removeEcipes
+   * @returns {object} removeEntry
    * @param {*} req
    * @param {*} res
    */
@@ -35,47 +35,6 @@ class Entries {
       error: true
     });
   }
-
-   /**
-   * @returns {Object} updateRecipes
-   * @param {*} req
-   * @param {*} res
-   */
-  static modifyEntry(req, res) {
-    let i = 0
-    while(i<entries.length){
-      if (entries[i].id === parseInt(req.params.id, 10)) {
-          entries[i].title = req.body.title;
-          entries[i].content = req.body.content;
-          entries[i].modified = 1;
-          return res.json({
-            entries,
-            message: 'entry modified successfully',
-            error: false
-          });
-        }
-      i++
-    }
-    return res.status(404).json({
-      message: 'entry not found',
-    });
-  }
-    static retrieveEntry(req, res) {
-      let i = 0;
-      while(i < entries.length){
-        if (entries[i].id === parseInt(req.params.id, 10)) {
-          return res.json({
-            entries: entries[i],
-            message: 'entry retrieved',
-          });
-        }
-        i++;
-      }
-      return res.status(404).json({
-        message: 'entry not found',
-        error: true
-      });
-    }
 
 }
 
