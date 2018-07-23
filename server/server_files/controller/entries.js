2180a80a6866cda45c18621f62eb296d7b20e522
Eze-odurukwe Adaeze <daizyodurukwe@gmail.com>
2018-07-23 07:38:21 +0100

    Update entries.js

--- server/server_files/controller/entries.js
+++ server/server_files/controller/entries.js
@@ -4,7 +4,7 @@ import entries from '../model/entries';
  */
 class Entries {
     /**
-     * @returns {Object} recipes
+     * @returns {Object} Entries
      * @param {*} req
      * @param {*} res
      */
