9812b057e0394527d92526d55a7ac2bd5b9d2659
Eze-odurukwe Adaeze <daizyodurukwe@gmail.com>
2018-07-23 07:41:29 +0100

    Update entries.js

--- server/server_files/controller/entries.js
+++ server/server_files/controller/entries.js
@@ -14,69 +14,10 @@ class Entries {
     });
   }
   /**
-   * @returns {object} removeEcipes
+   * @returns {object} Entry
    * @param {*} req
    * @param {*} res
    */
-  static deleteEntry(req, res) {
-    let i = 0;
-    while(i < entries.length){
-      if (entries[i].id === parseInt(req.params.id, 10)) {
-        entries.splice(i, 1);
-        return res.json({
-          message: 'entry removed',
-        });
-      }
-      i++;
-    }
-
-    return res.status(404).json({
-      message: 'entry not found',
-      error: true
-    });
-  }
-
-   /**
-   * @returns {Object} updateRecipes
-   * @param {*} req
-   * @param {*} res
-   */
-  static modifyEntry(req, res) {
-    let i = 0
-    while(i<entries.length){
-      if (entries[i].id === parseInt(req.params.id, 10)) {
-          entries[i].title = req.body.title;
-          entries[i].content = req.body.content;
-          entries[i].modified = 1;
-          return res.json({
-            entries,
-            message: 'entry modified successfully',
-            error: false
-          });
-        }
-      i++
-    }
-    return res.status(404).json({
-      message: 'entry not found',
-    });
-  }
-    static retrieveEntry(req, res) {
-      let i = 0;
-      while(i < entries.length){
-        if (entries[i].id === parseInt(req.params.id, 10)) {
-          return res.json({
-            entries: entries[i],
-            message: 'entry retrieved',
-          });
-        }
-        i++;
-      }
-      return res.status(404).json({
-        message: 'entry not found',
-        error: true
-      });
-    }
-
     static createEntry(req, res) {
         entries.push({
           id: entries.length + 1,
